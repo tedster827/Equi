aaa543c05710ba5db585a0a44c027543640d9da8
tedster827 <cteddyjr@icloud.com>
2016-08-02 19:56:35 -0700

    First Run! Display Vector!
    The first run had no errors, that’s always a great feeling! Adding
    display function to see if the inputs are getting adding to the input
    vector correctly.

--- client.cpp
+++ client.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+/*
+Debug: [Int Only] Display Input Vector.
+Purpose: To make sure the input vector is getting the
+correct inputs.
+Input: A [Input Vector-Passing by reference to save
+memory]
+Output: None [Void Function]
+*/
+void displayVector(vector<int> &A)
+{
+  if(A.size() > 0)
+  {
+    cout << "Input Vector: "
+    for(int i = 0; i < A.size(); i++)
+    {
+      cout << A[i] << " " << endl;
+    }
+  }
+  cout << "[Error] Cannot Display An Empty Vector."
+  return;
+}
+
+
 int main()
 {
   //array of numbers.
@@ -22,6 +45,9 @@ int main()
       A.push_back(gNumber);
     }
   }
+
+  //Debug: Display Input Vector.
+
 
   return 0;
 }
